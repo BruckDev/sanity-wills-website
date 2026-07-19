5e98d8175d762346235cd88b1a55b11534b1e8f8
Gordana Bruck <299949807+gbruckcpa@users.noreply.github.com>
2026-07-19 12:07:28 -0400

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
 import './globals.css'
+import Script from 'next/script'
 
 export default function RootLayout({children}: LayoutProps<'/'>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LE0CWC41BG" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-LE0CWC41BG');`}
+        </Script>
+      </body>
     </html>
   )
 }
